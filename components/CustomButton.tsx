c48881137a8c734066ead95cc91a831c56d2c3b6
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-17 14:44:36 -0300

    BUILT - Structure page configs and button initial

---
+++ components/CustomButton.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+"use client";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custon-btn`}
+      onClick={() => { }}
+    >
+    </button>
+  )
+}
+export default CustomButton
---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+
+import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+          Find, book or rent a car - quickly and easily!
+        </h1>
+        <p className='hero__subtitle'>
+          Streamline your rental experience with our effortle booking process.
+        </p>
+        <CustomButton />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
