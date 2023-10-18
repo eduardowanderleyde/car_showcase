1259405979adab860978d088c957a2e5dba7df91
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-18 14:36:02 -0300

    BUILT - config how to show Cars API information

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import { CustomFilter, Hero } from '@/components'
+import { CarCard, CustomFilter, Hero } from '@/components'
 import Image from 'next/image'
 import { SearchBar } from '@/components'
+import { fetchCars } from '@/utils';
 
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
-      <div className='mt-12 padding-x padding-y max-width' id="discover">
+
+      <div className='mt-12 padding-x padding-y
+      max-width' id="discover">
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
           <p>Explore the cars you might like</p>
@@ -15,13 +22,30 @@ export default function Home() {
 
         <div className="home__filters">
           <SearchBar />
+
           <div className="home__filter-container">
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
-
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+
+            </div>
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
+
     </main>
   )
 }
