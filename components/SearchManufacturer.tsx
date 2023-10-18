af37a106819d94de7d4e88d59eb9f6768f7cc15c
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-17 21:45:31 -0300

    BUILT - using useState to start the SearchBar

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,9 @@
+import { SearchManufacturerProps } from '@/types'
+
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  return (
+    <div>SearchManufacturer</div>
+  )
+}
+
+export default SearchManufacturer
