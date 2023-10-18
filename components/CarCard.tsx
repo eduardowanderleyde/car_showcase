6d0e6139b013a46f93a17c2eff85a6f713aeae11
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-18 15:34:39 -0300

    BUILT - Make the View More Fragment

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
+import { CarDetails } from '.';
 
 interface CarCardProps {
   car: CarProps;
@@ -13,6 +14,8 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
 
   const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -44,7 +47,31 @@ const CarCard = ({ car }: CarCardProps) => {
               {transmission === 'a' ? "Automatic" : 'Manual'}
             </p>
           </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className='text-[14px]'>
+              {drive.toUpperCase()}
+            </p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
+            <p className='text-[14px]'>
+              {city_mpg} MPG
+            </p>
+          </div>
         </div>
+        <div className='car-card__btn-container'>
+          <CustomButton
+            title="View More"
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+      <div>
+        <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
       </div>
     </div>
   )
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
@@ -13,9 +13,19 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomBu
       `}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>
+      <span className={`flex-1 ${textStyles}`}>
         {title}
       </span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
     </button>
   )
 }
