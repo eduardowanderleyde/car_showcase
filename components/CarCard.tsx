42612ead04f26c2706065d6b76646a4ebfc26dec
Eduardo Wanderley de Siqueira Araújo <eduardo.2020250145@unicap.br>
2023-10-18 14:59:56 -0300

    BUILT - image from the wheel in the end of the CarCard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -36,6 +36,16 @@ const CarCard = ({ car }: CarCardProps) => {
       <div className='relative w-full h-40 mt-3 object-contain'>
         <Image src="/hero.png" alt="car model" fill priority className='object-contain' />
       </div>
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? "Automatic" : 'Manual'}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
